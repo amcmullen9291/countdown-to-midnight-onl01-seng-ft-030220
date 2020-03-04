3895efcd3d435f641dfce01fd2d978dd99859a92
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-04 17:50:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
-
 def countdown
+  countdown=10
+  while countdown>0 do 
+     puts"#{countdown} second(S)!"
+    countdown-= 1
+    sleep 1 
+  end      
+     "HAPPY NEW YEAR!"
+end
+
+  def countdown
+  countdown=10
+  while countdown>0 do 
+     puts"#{countdown} second(S)!"
+    countdown-= 1
+    sleep 1 
+  end      
+     "HAPPY NEW YEAR!"
 end
